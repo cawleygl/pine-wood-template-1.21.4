f4fc0f4cf44936c298c07ddf084d0addf177f7e9
Grant Cawley <72057527+cawleygl@users.noreply.github.com>
2025-03-23 23:43:02 -0400

    Initial commit

---
+++ src/main/java/bluesteel42/pinewood/PineWoodClient.java
@@ -0,0 +1,10 @@
+package bluesteel42.pinewood;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PineWoodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/bluesteel42/pinewood/PineWoodDataGenerator.java
@@ -0,0 +1,11 @@
+package bluesteel42.pinewood;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PineWoodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
